d426bc09971b654c29c04caafb8b68dbe994193a
siniwyh <siniwyh@icloud.com>
2025-02-01 13:08:51 +0700

    code added

---
+++ 5.classes/task.js
@@ -0,0 +1,151 @@
+class PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		this.name = name;
+		this.releaseDate = releaseDate;
+		this.pagesCount = pagesCount;
+		this.state = 100;
+		this.type = null;
+	}
+
+	fix() {
+		if (this.state < 100 && this.state > 0) {
+			this.state = Math.min(100, Math.floor(this.state * 1.5));
+		}
+	}
+
+	set state(value) {
+		if (value < 0) {
+			this._state = 0;
+		} else if (value > 100) {
+			this._state = 100;
+		} else {
+			this._state = value;
+		}
+	}
+
+	get state() {
+		return this._state;
+	}
+}
+
+const sherlock = new PrintEditionItem(
+	"Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+	2019,
+	1008
+);
+
+console.log(sherlock.releaseDate); //2019
+console.log(sherlock.state); //100
+sherlock.fix();
+console.log(sherlock.state); //100
+
+class Magazine extends PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.type = "magazine";
+	}
+}
+
+class Book extends PrintEditionItem {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.author = author;
+		this.type = "book";
+	}
+}
+
+class NovelBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = "novel";
+	}
+}
+
+class FantasticBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = "fantastic";
+	}
+}
+
+class DetectiveBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = "detective";
+	}
+}
+
+const picknick = new FantasticBook(
+	"Аркадий и Борис Стругацкие",
+	"Пикник на обочине",
+	1972,
+	168
+);
+
+console.log(picknick.author); //"Аркадий и Борис Стругацкие"
+picknick.state = 10;
+console.log(picknick.state); //10
+picknick.fix();
+console.log(picknick.state); //15
+
+class Library {
+	constructor(name) {
+		this.name = name;
+		this.books = [];
+	}
+
+	addBook(book) {
+		if (book.state > 30) {
+			this.books.push(book);
+		}
+	}
+
+	findBookBy(type, value) {
+		for (let book of this.books) {
+			if (book[type] === value) {
+				return book;
+			}
+		}
+		return null;
+	}
+
+	giveBookByName(bookName) {
+		const bookIndex = this.books.findIndex(book => book.name === bookName);
+		if (bookIndex !== -1) {
+			return this.books.splice(bookIndex, 1)[0];
+		}
+		return null;
+	}
+}
+const library = new Library("Библиотека имени Ленина");
+
+library.addBook(
+	new DetectiveBook(
+		"Артур Конан Дойл",
+		"Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+		2019,
+		1008
+	)
+);
+library.addBook(
+	new FantasticBook(
+		"Аркадий и Борис Стругацкие",
+		"Пикник на обочине",
+		1972,
+		168
+	)
+);
+library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+library.addBook(new Magazine("Мурзилка", 1919, 60));
+
+console.log(library.findBookBy("name", "Властелин колец")); //null
+console.log(library.findBookBy("releaseDate", 1919).name); //"Мурзилка" - Найти книгу 1919 года)
+console.log("Количество книг до выдачи: " + library.books.length); //Количество книг до выдачи: 4
+const issuedBook = library.giveBookByName("Машина времени"); // выдаем книгу, записываем в константу
+issuedBook.state = 20; // повреждаем книгу
+console.log("Количество книг после выдачи: " + library.books.length); //Количество книг после выдачи: 3
+library.addBook(issuedBook); // добавляем книгу с состоянием 20
+console.log("Количество книг после попытки добавить поврежденную: " + library.books.length); //Количество книг после попытки добавить поврежденную: 3
+issuedBook.state = 40
+library.addBook(issuedBook); // добавляем книгу с состоянием 40
+console.log("Количество книг после попытки добавить поврежденную и затем восстановленную: " + library.books.length); //Количество книг после попытки добавить поврежденную и затем восстановленную: 4
